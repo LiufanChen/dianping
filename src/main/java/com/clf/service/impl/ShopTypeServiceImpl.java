c16ab6f3fa0d54e935c20735ebbfd375a1bebdaa
13626646811 <1216951702@qq.com>
2025-06-17 15:38:57 +0800

    店铺类型缓存实现

--- src/main/java/com/clf/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/clf/service/impl/ShopTypeServiceImpl.java
@@ -1,6 +1,11 @@
 package com.clf.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.clf.dto.Result;
+import com.clf.entity.Shop;
 import com.clf.entity.ShopType;
 import com.clf.mapper.ShopTypeMapper;
 import com.clf.service.IShopTypeService;
@@ -9,26 +14,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static cn.hutool.http.ContentType.JSON;
+import static com.clf.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
+import static com.clf.utils.RedisConstants.CACHE_SHOP_TYPE_TTL;
 
 /**
  * <p>
  *  服务实现类
  * </p>
  *
- * @author 虎哥
- * @since 2021-12-22
  */
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
-
+    @Resource
+    private IShopTypeService typeService;
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
     @Override
     public Result queryTypeList() {
 
-
+        String redisCacheShopType = stringRedisTemplate.opsForValue().get(CACHE_SHOP_TYPE_KEY);
+        if(StrUtil.isNotBlank(redisCacheShopType)){
+            List<ShopType> typeList = JSONUtil.toList(redisCacheShopType, ShopType.class);
+            log.debug("从Redis缓存中获取店铺类型列表，数据: "+typeList);
+            return Result.ok(typeList);
+        }
         List<ShopType> typeList = query().orderByAsc("sort").list();
+        if(typeList == null|| typeList.isEmpty()){
+            log.debug("从数据库查询店铺类型列表，但结果为空");
+            return Result.fail("获取店铺类型失败");
+        }
+        log.debug("从数据库查询店铺类型列表，数据: "+typeList);
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_TYPE_KEY,JSONUtil.toJsonStr(typeList),CACHE_SHOP_TYPE_TTL, TimeUnit.MINUTES);
         return Result.ok(typeList);
     }
 }
--- src/main/java/com/clf/utils/RedisConstants.java
+++ src/main/java/com/clf/utils/RedisConstants.java
@@ -15,7 +15,6 @@ public class RedisConstants {
     public static final Long LOGIN_USER_TTL = 36000L;
     public static final String CACHE_SHOP_KEY = "cache:shop:";
     public static final Long CACHE_SHOP_TTL = 30L;
-
     public static final String CACHE_SHOP_TYPE_KEY = "cache:shopType:";
     public static final Long CACHE_SHOP_TYPE_TTL = 30L;
 
