e9898fbbfe23f436cd61f48efcede93c255cd3d8
13626646811 <1216951702@qq.com>
2025-06-19 22:58:41 +0800

    秒杀超卖问题，一人一单。乐观锁修改库存，悲观锁增加订单，事务提交。

--- src/main/java/com/clf/HmDianPingApplication.java
+++ src/main/java/com/clf/HmDianPingApplication.java
@@ -3,7 +3,10 @@ package com.clf;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+
+@EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan("com.clf.mapper")
 @SpringBootApplication
 public class HmDianPingApplication {
--- src/main/java/com/clf/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/clf/service/impl/VoucherOrderServiceImpl.java
@@ -8,13 +8,16 @@ import com.clf.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.clf.utils.RedisIdWorker;
 import com.clf.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 /**
- *
+ *  乐观锁比较适合更新数据
+ *  插入数据需要使用悲观锁操作
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
@@ -23,34 +26,69 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private RedisIdWorker redisIdWorker;
     @Autowired
     private SeckillVoucherServiceImpl seckillVoucherService;
+
     @Override
     public Result seckillVoucher(Long voucherId) {
 
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        // 2.判断秒杀是否开始
         if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
             return Result.fail("秒杀尚未开始!");
         }
-        if(voucher.getEndTime().isBefore(LocalDateTime.now())) {
+        // 3.判断秒杀是否已经结束
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
             return Result.fail("秒杀已经结束！");
         }
-        if (voucher.getStock()<1) {
+        // 4.判断库存是否充足
+        if (voucher.getStock() < 1) {
             return Result.fail("库存不足");
         }
+        Long userId = UserHolder.getUser().getId();
 
-        boolean success= seckillVoucherService.update().setSql("stock = stock-1").eq("voucher_id", voucherId).update();
+        synchronized (userId.toString().intern()) {
+            //获取代理对象
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId, userId);
+        }
+    }
+//userId是Long类型，直接使用Long对象作为锁可能导致意外行为（如自动装箱缓存问题）synchronized的锁对象必须是Object类型
 
-        if(!success) {
+    //用到悲观锁
+    @Transactional
+    public Result createVoucherOrder(Long voucherId,Long userId) {
+
+//        Long userId = UserHolder.getUser().getId();
+        // 5.1.查询订单
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        // 5.2.判断是否存在
+        if (count > 0) {
+            // 用户已经购买过了
+            return Result.fail("用户已经购买过一次！");
+        }
+
+        // 6.扣减库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1") // set stock = stock - 1
+                .eq("voucher_id", voucherId).gt("stock", 0) // where id = ? and stock > 0
+                .update();
+        if (!success) {
+            // 扣减失败
             return Result.fail("库存不足！");
         }
-        //创建订单
+
+        // 7.创建订单
         VoucherOrder voucherOrder = new VoucherOrder();
+        // 7.1.订单id
         long orderId = redisIdWorker.nextId("order");
         voucherOrder.setId(orderId);
-        Long userId = UserHolder.getUser().getId();
+        // 7.2.用户id
         voucherOrder.setUserId(userId);
+        // 7.3.代金券id
         voucherOrder.setVoucherId(voucherId);
         save(voucherOrder);
 
+        // 7.返回订单id
         return Result.ok(orderId);
+
     }
 }
