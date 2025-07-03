2afd7e8eb5f4c353f80faf315a9a5e4d8ee61d41
13626646811 <1216951702@qq.com>
2025-07-03 23:13:19 +0800

    查询探店笔记

--- src/main/java/com/clf/service/IBlogService.java
+++ src/main/java/com/clf/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.clf.service;
 
+import com.clf.dto.Result;
 import com.clf.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -12,5 +13,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2021-12-22
  */
 public interface IBlogService extends IService<Blog> {
+
+
+    Result queryHotBlog(Integer current);
+
+    Result queryBlogById(Long id);
 
 }
--- src/main/java/com/clf/service/impl/BlogServiceImpl.java
+++ src/main/java/com/clf/service/impl/BlogServiceImpl.java
@@ -1,10 +1,18 @@
 package com.clf.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.clf.dto.Result;
 import com.clf.entity.Blog;
+import com.clf.entity.User;
 import com.clf.mapper.BlogMapper;
 import com.clf.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.clf.service.IUserService;
+import com.clf.utils.SystemConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +24,44 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
+
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(this::queryBlogUser);
+        return Result.ok(records);
+    }
+
+    @Override
+    public Result queryBlogById(Long id) {
+        Blog blog = getById(id);
+        if (blog == null) {
+            return Result.fail("笔记不存在！");
+        }
+        queryBlogUser(blog);
+        return Result.ok(blog);
+    }
+
+
+
+    private void queryBlogUser(Blog blog){
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+
+    }
+
+
+
 
 }
